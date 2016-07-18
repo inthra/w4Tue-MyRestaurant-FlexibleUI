635345bfd0713bf6936194ec4c8031647b725cfe
Ashley Maceli and Nick Lyman <ashley@>
2016-07-18 09:19:50 -0700

    add gesture interfaces, enable item touch helper in RecyclerView, add reorder icon to saved restaurants

---
+++ app/src/main/java/com/epicodus/myrestaurants/util/OnStartDragListener.java
@@ -0,0 +1,11 @@
+package com.epicodus.myrestaurants.util;
+
+import android.support.v7.widget.RecyclerView;
+import android.widget.AbsListView;
+
+/**
+ * Created by Guest on 7/18/16.
+ */
+public interface OnStartDragListener {
+    void onStartDrag(RecyclerView.ViewHolder viewHolder);
+}
