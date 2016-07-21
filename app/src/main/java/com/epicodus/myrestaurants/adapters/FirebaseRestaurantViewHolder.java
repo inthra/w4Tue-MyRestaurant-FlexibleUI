300f28da8c54d49eaa14e200d99d5180a0b2cbaa
Inthrayuth Mahaphol and Nick Lyman <inthrayuth@>
2016-07-21 14:51:41 -0700

    Add ability to use camera and save personalized pictures for individual saved restaurants. Images saved to Firebase.

--- app/src/main/java/com/epicodus/myrestaurants/adapters/FirebaseRestaurantViewHolder.java
+++ app/src/main/java/com/epicodus/myrestaurants/adapters/FirebaseRestaurantViewHolder.java
@@ -5,7 +5,10 @@ import android.animation.AnimatorInflater;
 import android.animation.AnimatorSet;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.widget.RecyclerView;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
@@ -25,6 +28,7 @@ import com.squareup.picasso.Picasso;
 
 import org.parceler.Parcels;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class FirebaseRestaurantViewHolder extends RecyclerView.ViewHolder implements ItemTouchHelperViewHolder {
@@ -47,15 +51,32 @@ public class FirebaseRestaurantViewHolder extends RecyclerView.ViewHolder implem
         TextView ratingTextView = (TextView) mView.findViewById(R.id.ratingTextView);
         mRestaurantImageView = (ImageView) mView.findViewById(R.id.restaurantImageView);
 
-        Picasso.with(mContext)
-                .load(restaurant.getImageUrl())
-                .resize(MAX_WIDTH, MAX_HEIGHT)
-                .centerCrop()
-                .into(mRestaurantImageView);
-
+        if (!restaurant.getImageUrl().contains("http")) {
+            try {
+                Bitmap imageBitmap = decodeFromFirebaseBase64(restaurant.getImageUrl());
+                mRestaurantImageView.setImageBitmap(imageBitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            // This block of code should already exist, we're just moving it to the 'else' statement:
+            Picasso.with(mContext)
+                    .load(restaurant.getImageUrl())
+                    .resize(MAX_WIDTH, MAX_HEIGHT)
+                    .centerCrop()
+                    .into(mRestaurantImageView);
+            nameTextView.setText(restaurant.getName());
+            categoryTextView.setText(restaurant.getCategories().get(0));
+            ratingTextView.setText("Rating: " + restaurant.getRating() + "/5");
+        }
         nameTextView.setText(restaurant.getName());
         categoryTextView.setText(restaurant.getCategories().get(0));
         ratingTextView.setText("Rating: " + restaurant.getRating() + "/5");
+    }
+
+    public static Bitmap decodeFromFirebaseBase64(String image) throws IOException {
+        byte[] decodeByteArray = android.util.Base64.decode(image, Base64.DEFAULT);
+        return BitmapFactory.decodeByteArray(decodeByteArray, 0 , decodeByteArray.length);
     }
 
 //    @Override
